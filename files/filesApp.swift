a550b78c51b83b976bd137a088106145a98014b1
Mineek <mineek@mineek.nl>
2022-12-28 23:12:26 +0100

    Initial Commit

---
+++ files/filesApp.swift
@@ -0,0 +1,17 @@
+//
+//  filesApp.swift
+//  files
+//
+//  Created by Mineek on 28/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct filesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
